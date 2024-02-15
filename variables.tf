6333af19fb8b20d22e3f598f186f1168b80ce2cb
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-02-14 21:55:31 -0500

    Implemented count meta attribute to provision multiple network resources

--- variables.tf
+++ variables.tf
@@ -19,11 +19,46 @@ variable "zone" {
 }
 
 variable "webapp-subnet-cidr-range" {
-  type = string
+  type    = string
   default = "10.1.0.0/24"
 }
 
 variable "db-subnet-cidr-range" {
-  type = string
+  type    = string
   default = "10.1.1.0/24"
+}
+
+variable "vpc-name" {
+  type    = string
+  default = "csye-vpc"
+}
+
+variable "webapp-subnet-name" {
+  type    = string
+  default = "webapp-subnet"
+}
+
+variable "db-subnet-name" {
+  type    = string
+  default = "db-subnet"
+}
+
+variable "vpc-count" {
+  type    = number
+  default = 1
+}
+
+variable "public-route-name" {
+  type = string
+  default = "public-route-for-webapp"
+}
+
+variable "cidr-range" {
+  type = string
+  default = "10.1.0.0/20"
+}
+
+variable "number-of-subnets" {
+  type = number
+  default = 2
 }
